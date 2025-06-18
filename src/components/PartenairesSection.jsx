709d9af1a501f454f9fb3f050c15679c623b5d6e
Ibrahim ANIL <ianil@novances-it.com>
2025-06-18 20:37:34 +0200

    Ajout des partenaires

--- src/components/PartenairesSection.jsx
+++ src/components/PartenairesSection.jsx
@@ -1,16 +1,30 @@
+import tcl from '../assets/img/tcl.png';
+import carrefour from '../assets/img/carrefour.png';
+import fnac from '../assets/img/fnac.png';
+import pathe from '../assets/img/pathe.png';
+
+
+
 const PartenairesSection = () => {
-  const partners = ["../assets/img/tcl.png", "/logos/tcl.png", "/logos/pathe.png", "/logos/fnac.png"];
+  const partners = [carrefour, tcl, pathe, fnac];
 
   return (
     <section className="p-6">
       <h2 className="text-xl font-semibold mb-4">Partenaires</h2>
-      <div className="flex flex-wrap justify-around gap-4">
+      <div className="flex justify-around items-center">
         {partners.map((logo, index) => (
-          <img key={index} src={logo} alt="Partenaire" className="h-10" />
+          <img
+            key={index}
+            src={logo}
+            alt="Partenaire"
+            className="max-w-[22%] h-auto object-contain"
+          />
         ))}
       </div>
+
     </section>
   );
 };
+
 
 export default PartenairesSection;
